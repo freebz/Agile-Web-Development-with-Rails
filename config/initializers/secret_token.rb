c0f0ee9e6d71302eae237401aa8102fe4eed6e66
FreeBz <freebz@hananet.net>
2013-04-15 16:11:31 +0900

    레일스 애플리케이션 생성하기

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '78f82d8d36a11522cdd0973dc5907fb38e7107228ee40d53eb06f2b903053e1cd66c348d4fb9939c426629c9f87fc221e07f7e2b8ac1a2cafc8380afa0a9d83d'
