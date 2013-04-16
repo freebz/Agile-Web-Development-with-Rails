33d5cb3868f1c1dafcc14b6781fe4843848a4ae1
FreeBz <freebz@hananet.net>
2013-04-16 11:32:26 +0900

    이터레이션 D2: 상품을 장바구니에 연결하기

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416015712) do
+ActiveRecord::Schema.define(:version => 20130416021405) do
 
   create_table "cards", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
